3b11291b9f5d000f16449a623c2c4de9a9902312
Anaïs <c7anais@gmail.com>
2021-09-17 17:01:18 +0200

    checking user's choice

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 alert("Welcome to the Chi-Fou-Mi Game. Are you ready to play?"); // welcoming message
 
 var userName = prompt("Please enter your name?") // asking for user's name
-// checking userName
+// checking user's name
 while (userName.length < 2 || userName.length > 20) {
     userName = prompt("Please enter your name?")
 } 
@@ -16,6 +16,13 @@ let computerScore = 0;
 
 while (userScore < 3 && computerScore < 3) { // loop for three victories
     var userChoice = prompt("It's your turn to play " + userName + "\nPlease make your choice between Rock, Paper and Scissors."); // asking for user's choice
+    // checking user's choice
+    userChoice = userChoice.toLowerCase();
+    while (Object.keys(choice).includes(userChoice) === false) {
+        userChoice = prompt("It's your turn to play " + userName + "\nPlease make your choice between Rock, Paper and Scissors.");
+        userChoice = userChoice.toLowerCase();
+    }
+
     var computerChoice = Object.keys(choice)[Math.floor(Math.random() * Object.keys(choice).length)];
     alert("I choose: " + computerChoice); // showing computer's choice
     
