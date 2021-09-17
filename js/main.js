3d496afa5ea9debbde5dd046207b644ca4e05726
Anaïs <c7anais@gmail.com>
2021-09-17 13:56:20 +0200

    add 3 victories to win the game

--- js/main.js
+++ js/main.js
@@ -2,25 +2,37 @@ alert("Welcome to the Chi-Fou-Mi Game, are you ready?"); // welcoming message
 
 var userName = prompt("Please enter your name?"); // asking for user's name
 
-var userChoice = prompt("It's your turn to play " + userName + "\nPlease make your choice between Rock, Paper and Scissors."); // asking for user's choice
-
 var choice = {
     "Rock" : ["Scissors", "Rock", "Paper"],
     "Paper" : ["Rock", "Paper", "Scissors"],
     "Scissors" : ["Paper", "Scissors", "Rock"]
 }
+let userScore = 0;
+let computerScore = 0;
 
-var computerChoice = Object.keys(choice)[Math.floor(Math.random() * Object.keys(choice).length)];
-alert("I choose: " + computerChoice); // showing computer's choice
-
-computerChoice = choice[userChoice].indexOf(computerChoice); // transforming computerChoice in number by using index of userChoice's property
-userChoice = choice[userChoice].indexOf(userChoice); // same as computerChoice
-
-// Comparing values to determine the winner
-if (userChoice === computerChoice) {
-    alert("It's a draw");
-} else if (userChoice > computerChoice) {
-    alert("Congratulations you win");
-} else {
-    alert("Sorry you lost")
+while (userScore < 3 && computerScore < 3) { // loop for three victories
+    var userChoice = prompt("It's your turn to play " + userName + "\nPlease make your choice between Rock, Paper and Scissors."); // asking for user's choice
+    var computerChoice = Object.keys(choice)[Math.floor(Math.random() * Object.keys(choice).length)];
+    alert("I choose: " + computerChoice); // showing computer's choice
+    
+    computerChoice = choice[userChoice].indexOf(computerChoice); // transforming computerChoice in number by using index of userChoice's property
+    userChoice = choice[userChoice].indexOf(userChoice); // same as computerChoice
+    
+    // comparing values to determine the winner
+    if (userChoice === computerChoice) {
+        alert("It's a draw\n"+ userName + " : " + userScore + " / computer : " + computerScore);
+    } else if (userChoice > computerChoice) {
+        userScore++
+        alert("You win this round\n" + userName + " : " + userScore + " / computer : " + computerScore);
+    } else {
+        computerScore++
+        alert("Sorry you lost this round\n"+ userName + " : " + userScore + " / computer : " + computerScore)
+    }
 }
+// end of the game
+if (userScore < computerScore){
+    alert("GAME OVER");
+} else {
+    alert("Congratulations you are the KING of Chi-Fou-Mi");
+}
+
